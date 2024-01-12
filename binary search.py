3232c95936fe2f67613f15365f8d177b7b2b7df5
Aastik <aastikdude7@gmail.com>
2024-01-12 10:16:15 -0800

    example binary search

--- binary search.py
+++ binary search.py
@@ -3,7 +3,7 @@
 def binary_search(arr , target ):
     middle = 0
     start = 0 
-    end = len(list)
+    end = len(arr)
     steps = 0 
 
     while(start<=end):
@@ -12,10 +12,17 @@ def binary_search(arr , target ):
         steps = steps+1
         middle = (start + end) //2
 
-        if target == list[middle]:
+        if target == arr[middle]:
             return middle
-        if target < list[middle]:
+        if target < arr[middle]:
             end = middle-1
         else:
             start = middle + 1
+    return -1
 
+# Example
+
+my_arr = [ 1,2,3,4,5,6,7,8,9,10]
+target = 2
+
+binary_search(my_arr,target)
