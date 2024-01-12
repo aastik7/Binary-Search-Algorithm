8ba23bd0360396db9b2493e90c4254a12b879d07
Aastik <aastikdude7@gmail.com>
2024-01-12 10:10:17 -0800

    Target search

--- binary search.py
+++ binary search.py
@@ -1,10 +1,21 @@
 
 
-def binary_search(list,elements):
+def binary_search(arr , target ):
     middle = 0
     start = 0 
     end = len(list)
     steps = 0 
 
     while(start<=end):
-    
+        print("Step", steps, ":" , str(arr[start:end+1]))
+
+        steps = steps+1
+        middle = (start + end) //2
+
+        if target == list[middle]:
+            return middle
+        if target < list[middle]:
+            end = middle-1
+        else:
+            start = middle + 1
+
